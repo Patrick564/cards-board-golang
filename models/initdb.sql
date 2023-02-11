c53b87b6d043af37eb25cd5bee249e3679f7576e
Patrick <pvilchez794@gmail.com>
2023-02-10 22:40:33 -0500

    Schema initialize update with boards and cards tables

--- models/initdb.sql
+++ models/initdb.sql
@@ -1,6 +1,22 @@
 CREATE TABLE IF NOT EXISTS users (
   id         UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-  email      STRING(100) UNIQUE NOT NULL,
+  email      STRING(100) NOT NULL UNIQUE,
   password   STRING(250) NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp()
+);
+
+CREATE TABLE IF NOT EXISTS boards (
+  id         UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+  name       STRING(50),
+  content    STRING(250),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp(),
+  id_user    UUID NOT NULL REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS cards (
+  id         UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+  content    STRING(250),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp(),
+  id_user    UUID NOT NULL REFERENCES users (id),
+  id_board   UUID NOT NULL REFERENCES boards (id)
 );
